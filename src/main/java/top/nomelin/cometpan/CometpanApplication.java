fbe9f9c8e7878b9bc1016cfcd30dbbc46c73634e
nomelin <2023686595@qq.com>
2024-04-07 20:39:18 +0800

    0.0.1alpha

---
+++ src/main/java/top/nomelin/cometpan/CometpanApplication.java
@@ -0,0 +1,13 @@
+package top.nomelin.cometpan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CometpanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CometpanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/nomelin/cometpan/CometpanApplicationTests.java
@@ -0,0 +1,13 @@
+package top.nomelin.cometpan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CometpanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
